e725bb72bb2f291fd47f0c59c634c2847a071621
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-07 23:35:19 -0400

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -4,36 +4,39 @@ from pyspark.sql.functions import col, lit, udf
 from pyspark.sql.types import FloatType
 
 spark = SparkSession.builder.appName('testing').getOrCreate()
+df= spark.read.csv('/Testing_df - Sheet1.csv', header = True, inferSchema = True)
 
-def get_data():
-    list_= []
-    for i in [float(j) / 100 for j in range(0, 100, 1)]:
-        list_.append(i) 
-    df = spark.createDataFrame(list_, FloatType()).toDF('prop')
-    df = df.withColumn('Target', target_udf(col('prop')))
-    df = df.withColumn('Prediction', prediction_udf(col('prop')))
-    return df
-def target_(x):
-    if x> 0.69:
-        return 1
-    elif x==0.62 or x==0.63 or x==0.55 or x==0.56 or x==0.57 or x==0.58 or x==0.59 or x==0.45 or x==0.46 or x==0.39:
-        return 1
-    else:
-        return 0
-target_udf = udf(lambda x : target_(x))
-def prediction_(x):
-    if x> 0.69:
-        return 1
-    elif x==0.62 or x==0.63 or x==0.55 or x==0.56 or x==0.57 or x==0.58 or x==0.59 or x==0.45 or x==0.47 or x==0.39:
-        return 1
-    else:
-        return 0
-prediction_udf = udf(lambda x : prediction_(x))
+
+
+# def get_data():
+#     list_= []
+#     for i in [float(j) / 100 for j in range(0, 100, 1)]:
+#         list_.append(i) 
+#     df = spark.createDataFrame(list_, FloatType()).toDF('prop')
+#     df = df.withColumn('Target', target_udf(col('prop')))
+#     df = df.withColumn('Prediction', prediction_udf(col('prop')))
+#     return df
+# def target_(x):
+#     if x> 0.69:
+#         return 1
+#     elif x==0.62 or x==0.63 or x==0.55 or x==0.56 or x==0.57 or x==0.58 or x==0.59 or x==0.45 or x==0.46 or x==0.39:
+#         return 1
+#     else:
+#         return 0
+# target_udf = udf(lambda x : target_(x))
+# def prediction_(x):
+#     if x> 0.69:
+#         return 1
+#     elif x==0.62 or x==0.63 or x==0.55 or x==0.56 or x==0.57 or x==0.58 or x==0.59 or x==0.45 or x==0.47 or x==0.39:
+#         return 1
+#     else:
+#         return 0
+# prediction_udf = udf(lambda x : prediction_(x))
 
 
 class Thresholdunit(object):
     def __init__(self,spark= spark,Target= None ,Probability = 'Prediction_Prob_1', prediction_col = 'prediction'):
-        self.df= get_data()
+#         self.df= get_data()
         self.threshold = ThresholdTuning(spark = spark,dataframe=self.df,MaxMisclassification_tolerence=0.04, expected_FalseAlertReduction = 0.4, 
                 buckets=10, MaxMisclassification_tolerence_local = None, NeedComputation = True, regulater_factor=0.0001,Target= 'Target',Probability = 'prop', recall_limit= 0.75, prediction_col = 'prediction' )
         
