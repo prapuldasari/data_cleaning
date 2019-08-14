c37c01babf47743403609e41bb8a80c54fd25c70
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-14 10:34:49 -0400

    Update run_unit_test.py

--- run_unit_test.py
+++ run_unit_test.py
@@ -3,6 +3,7 @@ from unit_test import Thresholdunit
 class UnitTest(object):
 
     def run(self):
+        print ('new files are being tested')
         t1 = Thresholdunit()
         t1.all_combined()
 
