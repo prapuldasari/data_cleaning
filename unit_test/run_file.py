af94c4ebf48ce27e668d4dee75a0d0ac18491fa1
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-15 11:12:00 -0400

    Update run_file.py

--- unit_test/run_file.py
+++ unit_test/run_file.py
@@ -1,6 +1,7 @@
 import unittest
+from test_1 import TestAdd
 
 if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover('.')
-    unittest.TextTestRunner(verbosity=2, failfast= True).run(testsuite)
+#     testsuite = unittest.TestLoader().discover('.')
+#     unittest.TextTestRunner(verbosity=2, failfast= True).run(testsuite)
     unittest.main()
