bd66d8f9d1b48c74dc4661a1287066c6e6a0f4a6
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-15 09:27:13 -0400

    Create test_1.py

---
+++ unit_test/test_1.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class TestAdd(unittest.TestCase):
+    """
+    Test the add function from the mymath library
+    """
+
+    def test_add_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = 1+2
+        self.assertEqual(result, 3)
+
+    def test_add_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = 10.5+2
+        self.assertEqual(result, 12.5)
+
+    def test_add_strings(self):
+        """
+        Test the addition of two strings returns the two string as one
+        concatenated string
+        """
+        result = 'abc' + 'def'
+        self.assertEqual(result, 'abcdef')
+
+    print ('Done')
+
+
