504963c08b3903aedd59c85114067714bd6d4911
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-15 12:01:15 -0400

    Update run_file.py

--- unit_test/run_file.py
+++ unit_test/run_file.py
@@ -3,5 +3,5 @@ import sys
 
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('.')
-    unittest.TextTestRunner(verbosity=2, failfast= True).run(testsuite)
+    result = unittest.TextTestRunner(verbosity=2, failfast= True).run(testsuite)
     sys.exit(not result.wasSuccessful())
