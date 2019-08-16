3f65e26fde11d1be51ba8e0e8a7135fb644f2563
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-16 03:23:06 -0400

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -3,6 +3,24 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+__all__ = []
+
+import pkgutil
+import inspect
+
+for loader, name, is_pkg in pkgutil.walk_packages([sys.path]):
+    module = loader.find_module(name).load_module(name)
+
+    for name, value in inspect.getmembers(module):
+        if name.startswith('__'):
+            continue
+
+        globals()[name] = value
+        __all__.append(name)
+
+print (sys.path)
+print (__all__)
+
 from tm.tm_1 import *
 if __name__ == '__main__':
      unittest.main()
