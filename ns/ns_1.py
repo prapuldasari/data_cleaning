deec7ab909ee20fe925ff9bf36fb1e9b773554c6
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-16 03:47:03 -0400

    Update ns_1.py

--- ns/ns_1.py
+++ ns/ns_1.py
@@ -1,26 +1,26 @@
 import unittest
 
 
-class ns_2(unittest.TestCase):
+class ns_1(unittest.TestCase):
     """
     Test the add function from the mymath library
     """
 
-    def test_ns_2_add_integers(self):
+    def test_ns_1_add_integers(self):
         """
         Test that the addition of two integers returns the correct total
         """
         result = 1+2
         self.assertEqual(result, 3)
 
-    def test_ns_2_add_floats(self):
+    def test_ns_1_add_floats(self):
         """
         Test that the addition of two floats returns the correct result
         """
         result = 10.5+2
         self.assertEqual(result, 12.5)
 
-    def test_ns_2_add_strings(self):
+    def tes_ns_2_add_strings(self):
         """
         Test the addition of two strings returns the two string as one
         concatenated string
