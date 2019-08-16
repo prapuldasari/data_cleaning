6b4583349cf32ab1731374a996962d32e732a1f7
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-16 00:24:39 -0400

    Add files via upload

---
+++ tm/tm_2.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class tm_2(unittest.TestCase):
+    """
+    Test the add function from the mymath library
+    """
+
+    def test_tm_2_add_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = 1+2
+        self.assertEqual(result, 3)
+
+    def test_tm_2_add_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = 10.5+2
+        self.assertEqual(result, 12.5)
+
+    def test_tm_2_add_strings(self):
+        """
+        Test the addition of two strings returns the two string as one
+        concatenated string
+        """
+        result = 'abc' + 'def'
+        
+        print ('result')
+        self.assertEqual(result, 'abcdef')
+
+if __name__ == '__main__':
+     unittest.main()
