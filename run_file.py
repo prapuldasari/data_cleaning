5c9e8610261f9fc609f28ee329f3ec871178e521
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-16 00:31:27 -0400

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -1,3 +1,8 @@
 import unittest
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from tm import *
 if __name__ == '__main__':
      unittest.main(verbosity =2)
