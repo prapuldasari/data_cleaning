a0577344bec03330f1b14e5669fb5d7284234f3d
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-16 00:38:54 -0400

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -3,6 +3,6 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from tm import *
+from tm.tm_1 import *
 if __name__ == '__main__':
-     unittest.main(verbosity =2)
+     python -m unittest discover --pattern=*.py
