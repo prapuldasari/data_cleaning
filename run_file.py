5abfaa8c9f6aa3501f2c48ca0f7e0df05b579ae7
prapuldasari <39596546+prapuldasari@users.noreply.github.com>
2019-08-20 08:13:46 -0400

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -4,17 +4,12 @@ import pkgutil
 import inspect
 import sys
 for loader, name, is_pkg in pkgutil.walk_packages(['/Users/pk/.jenkins/workspace/testing_sh/tm', '/Users/pk/.jenkins/workspace/testing_sh/ns']):
-    print ('hereer')
     module = loader.find_module(name).load_module(name)
-    print ('here 1', module)
     for name, value in inspect.getmembers(module):
-        print ('here 2', name)
         if name.startswith('__'):
             continue
         globals()[name] = value
         __all__.append(name)
-print (sys.path)
-print (__all__)
 if __name__ == '__main__':
      unittest.main(verbosity= 2)
 # **
